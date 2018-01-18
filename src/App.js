3c88e5fb9aa0fd1d472365be3b10dbb31de8e2b0
charliechauri <carlos.echauri@gmail.com>
2018-01-18 13:14:27 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        Photo gallery
+      </div>
+    );
+  }
+}
+
+export default App;
