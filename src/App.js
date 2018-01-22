6f0adfb94109cf64cee08cc3d651eae304a69502
charliechauri <carlos.echauri@gmail.com>
2018-01-21 19:56:40 -0600

    feat Add API call to photos and set gallery

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import PhotoGallery from './components/PhotoGallery/PhotoGallery';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { photosDb: [] };
+
+    axios
+      .get('https://jsonplaceholder.typicode.com/photos')
+      .then((response) => {
+        this.setState({
+          photosDb: response.data,
+        });
+      });
+  }
+
   render() {
+    /* const filteredPhotos = this.state.photosDb.filter(photo => photo.albumId === 1); */
+    const filteredPhotos = this.state.photosDb;
+
     return (
-      <div>
-        Photo gallery
-      </div>
+      <section>
+
+        <header>
+          <h1 className="text-center">Photo Gallery</h1>
+        </header>
+
+        <PhotoGallery photos={filteredPhotos} />
+      </section>
     );
   }
 }
